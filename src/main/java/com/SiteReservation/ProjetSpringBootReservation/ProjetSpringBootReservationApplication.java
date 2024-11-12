afbfa988e8243088d3c8a174ec9b930ec05e6a8e
Omar <omarahl9950@gmail.com>
2024-11-12 16:33:16 +0100

    1first commit

---
+++ src/main/java/com/SiteReservation/ProjetSpringBootReservation/ProjetSpringBootReservationApplication.java
@@ -0,0 +1,13 @@
+package com.SiteReservation.ProjetSpringBootReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSpringBootReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetSpringBootReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SiteReservation/ProjetSpringBootReservation/ProjetSpringBootReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SiteReservation.ProjetSpringBootReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringBootReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
