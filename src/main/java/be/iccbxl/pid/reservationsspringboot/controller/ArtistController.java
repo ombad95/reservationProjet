075508c00aa63d4863fbcdaa6022a5a9351933d9
Omar <omarahl9950@gmail.com>
2024-11-13 15:39:45 +0100

    Form Delete Artist

--- src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import be.iccbxl.pid.reservationsspringboot.model.Artist;
 import be.iccbxl.pid.reservationsspringboot.service.ArtistService;
 import jakarta.servlet.http.HttpServletRequest;
+
 
 
 
@@ -100,6 +102,17 @@ public class ArtistController {
 
         return "redirect:/artists/"+artist.getId();
     }
+    @DeleteMapping("/artists/{id}")
+    public String delete(@PathVariable("id") long id, Model model) {
+        Artist existing = service.getArtist(id);
+
+        if(existing!=null) {
+            service.deleteArtist(id);
+        }
+
+        return "redirect:/artists";
+    }
+
 
 
 }
