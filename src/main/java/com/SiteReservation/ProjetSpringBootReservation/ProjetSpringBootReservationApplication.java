c2614c345f12ff328783cd37410f5b7e43d8d05a
Omar <omarahl9950@gmail.com>
2024-11-13 01:39:21 +0100

    Controller Artist +templates +routes

--- src/main/java/com/SiteReservation/ProjetSpringBootReservation/ProjetSpringBootReservationApplication.java
+++ src/main/java/com/SiteReservation/ProjetSpringBootReservation/ProjetSpringBootReservationApplication.java
@@ -3,7 +3,7 @@ package com.SiteReservation.ProjetSpringBootReservation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.SiteReservation.ProjetSpringBootReservation")
 public class ProjetSpringBootReservationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/SiteReservation/ProjetSpringBootReservation/model/Artist.java
+++ src/main/java/com/SiteReservation/ProjetSpringBootReservation/model/Artist.java
@@ -5,9 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name="artists")
+
 public class Artist {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -19,30 +24,6 @@ public class Artist {
 
     public Artist(String firstname, String lastname) {
         this.firstname = firstname;
-        this.lastname = lastname;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
