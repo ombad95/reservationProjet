c0bd9138f9110e8393146a981f3ed74ed28a9d9f
Omar <omarahl9950@gmail.com>
2024-11-13 16:35:25 +0100

    Protection contre les attaques CSRF

---
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/config/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package be.iccbxl.pid.reservationsspringboot.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import be.iccbxl.pid.reservationsspringboot.model.User;
+import be.iccbxl.pid.reservationsspringboot.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByLogin(username);
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getLogin(), user.getPassword(),
+                getGrantedAuthorities(user.getRole()));
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(String role) {
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+        return authorities;
+    }
+}
