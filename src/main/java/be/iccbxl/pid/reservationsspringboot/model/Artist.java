3623b6d4d39d64498c8b67bea8512859c63b31f6
Omar <omarahl9950@gmail.com>
2025-02-11 22:51:13 +0100

    Added notification system

--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/Artist.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/Artist.java
@@ -5,40 +5,30 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
+
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 
-@Setter
-@Getter
 @Entity
 @Table(name="artists")
 public class Artist {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message = "The firstname must not be empty.")
-    @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
 
+    @NotEmpty(message = "The firstname must not be empty.")
+    @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
     private String firstname;
-    @NotBlank(message = "The lastname must not be empty.")
-    @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
 
+    @NotEmpty(message = "The lastname must not be empty.")
+    @Size(min=2, max=60, message = "The firstname must be between 2 and 60 characters long.")
     private String lastname;
 
-    protected Artist() {}
-
-    public Artist(String firstname, String lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-    }
-
-    @Override
-    public String toString() {
-        return firstname + " " + lastname;
-    }
 
 }
 
